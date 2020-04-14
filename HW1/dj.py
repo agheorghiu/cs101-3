12be9c895d81759ae72c422219ad8e9da577c308
Andru <androidus@gmail.com>
2020-04-14 14:45:58 -0700

    Changed types in dj.py

--- HW1/dj.py
+++ HW1/dj.py
@@ -1,30 +1,32 @@
 from interf import *
+from helpers import *
 
 numBits = 5
 
-def constant0(x: int) -> int:
+def constant0(x: BinStr) -> int:
     return 0
 
-def constant1(x: int) -> int:
+def constant1(x: BinStr) -> int:
     return 1
 
-def balanced1(x: int) -> int:
-    return x % 2
+def balanced1(x: BinStr) -> int:
+    return toInt(x) % 2
 
-def balanced2(x: int) -> int:
-    if (x < (2**numBits)/2):
+def balanced2(x: BinStr) -> int:
+    if (toInt(x) < (2**numBits)/2):
         return 0
     return 1
 
-def balanced3(x: int) -> int:
+def balanced3(x: BinStr) -> int:
     return 1 - balanced2(x)
 
-def balanced4(x: int) -> int:
-    if (x < (2**numBits) / 4):
+def balanced4(x: BinStr) -> int:
+    xi = toInt(x)
+    if (xi < (2**numBits) / 4):
         return 0
-    elif (x < (2**numBits) / 2):
+    elif (xi < (2**numBits) / 2):
         return 1
-    elif (x < 3*(2**numBits) / 4):
+    elif (xi < 3*(2**numBits) / 4):
         return 0
     return 1
 
