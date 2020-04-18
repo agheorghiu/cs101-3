f1f0fd4a49a2f6978de50a9a4c88ed15089efb0b
Andru <androidus@gmail.com>
2020-04-17 17:35:41 -0700

    Updated functions in periodfinding.py

--- HW1/periodfinding.py
+++ HW1/periodfinding.py
@@ -1,8 +1,7 @@
 from interf import *
 from helpers import *
 
-# note that changing this d will change the periods of some of the functions below
-d = 43
+d = 1024
 
 # has period 1
 def f0(x: int) -> int:
@@ -12,18 +11,18 @@ def f0(x: int) -> int:
 def f1(x: int) -> int:
     return (x % 2) + 1
 
-# has period 5
+# has period 4
 def f2(x: int) -> int:
-    return (x % 5) + 1
+    return (x % 4) + 1
 
 # has period 14
 def f3(x: int) -> int:
-    return (2**x) % d
+    return (2**x) % 43
 
 # has period 42
 def f4(x: int) -> int:
-    return (3**x) % d
+    return (3**x) % 43
 
-# has period 6
+# has period 16
 def f5(x: int) -> int:
-    return (3**(x**2)) % d
+    return (3**x) % 64
